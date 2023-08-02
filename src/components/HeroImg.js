00b0f7cc0383db7979730b800d4a0ec3dd414d5a
Rodney M Edge Jr <rodneymedgejr@Rodneys-MBP-2.attlocal.net>
2023-08-02 15:32:36 -0400

    finished the hero page and changed cursor for hamburger

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -7,16 +7,15 @@ const HeroImg = () => {
   return (
     <div className='hero'>
       <div className='mask'>
-        <img className='intro-img' src={IntroImg} alt = "IntroImg"/>
+        <img className='intro-img' src={IntroImg} alt="IntroImg" />
       </div>
       <div className='content'>
         <p>I AM A FREELANCER</p>
         <h1>Full-Stack Developer</h1>
-      </div>
-        <Link to='/projects' className='btn'>Projects</Link>
-        <Link to='/contact' className='btn-light'>Projects</Link>
-      <div>
-
+        <div>
+          <Link to='/projects' className="btn">Projects</Link>
+          <Link to='/contact' className="btn-light">Contact</Link>
+        </div>
       </div>
     </div>
   )
