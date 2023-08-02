ed88001eec2403cd2563cf103f7adb65a548d9d7
Rodney M Edge Jr <rodneymedgejr@Rodneys-MBP-2.attlocal.net>
2023-08-02 13:16:47 -0400

    added buttons to jsx and uninstall react-bootstrap

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -1,13 +1,23 @@
 import './HeroImg.css';
-
-import React from 'react'
+import React from 'react';
+import IntroImg from "../assets/complete-guide.jpg"
+import { Link } from 'react-router-dom';
 
 const HeroImg = () => {
   return (
     <div className='hero'>
-        <div className='mask'>
+      <div className='mask'>
+        <img className='intro-img' src={IntroImg} alt = "IntroImg"/>
+      </div>
+      <div className='content'>
+        <p>I AM A FREELANCER</p>
+        <h1>Full-Stack Developer</h1>
+      </div>
+        <Link to='/projects' className='btn'>Projects</Link>
+        <Link to='/contact' className='btn-light'>Projects</Link>
+      <div>
 
-        </div>
+      </div>
     </div>
   )
 }
