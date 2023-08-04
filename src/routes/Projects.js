d09eaa54513b542ba0fef1be5bf867ca391b57fb
Rodney M Edge Jr <rodneymedgejr@Rodneys-MBP-2.attlocal.net>
2023-08-04 14:09:38 -0400

    finished footer for now

--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 
 const Projects = () => {
   return (
     <div>
       <Navbar />
-      
+      <Footer />
     </div>
   )
 }
