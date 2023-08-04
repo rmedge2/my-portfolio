8418595341273f198ee392ee3854deede24592fd
Rodney M Edge Jr <rodneymedgejr@Rodneys-MBP-2.attlocal.net>
2023-08-04 15:41:08 -0400

    more progress on portfolio

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import HeroImg2 from '../components/HeroImg2'
 
 const About = () => {
   return (
     <div>
       <Navbar />
+      <HeroImg2 heading="ABOUT ME" text="here is a story about me"/>
       <Footer />
     </div>
   )
--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import HeroImg2 from '../components/HeroImg2'
 
 
 const Projects = () => {
   return (
     <div>
       <Navbar />
+      <HeroImg2 heading="Projects" text="Some of my works during my coding journey"/>
       <Footer />
     </div>
   )
