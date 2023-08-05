7a69f2c738a71e1afe840694eb49f5de3b3bd7ed
Rodney M Edge Jr <rodneymedgejr@Rodneys-MBP-2.attlocal.net>
2023-08-05 00:43:17 -0400

    fixed duplicate key:

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -13,7 +13,6 @@ const ProjectCardData = [
         title: "Netflix and Grill",
         text: "Netflix & Grill is a date night idea. The premis of the date includes a couple that decides to cook a meal together, and then watch a movie. The app will then generate 3 recipe options (with links to the recipes) alongside 3 movie options (with links to the movie previews)",
         view: "https://rmedge2.github.io/netflix-and-grill/",
-        view: "https://rmedge2.github.io/netflix-and-grill/",
         source: 'https://github.com/rmedge2/netflix-and-grill'
     },
     {
