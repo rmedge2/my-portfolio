00ec10fb9ff1932d3b7b1277a65c645165f45379
Rodney M Edge Jr <rodneymedgejr@Rodneys-MBP-2.attlocal.net>
2023-10-05 12:47:36 -0400

    added links to github and linkedin logos

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./Footer.css"
-import {  FaGithub, FaHome, FaLinkedin, FaMailBulk, FaPhone } from 'react-icons/fa'
+import { FaGithub, FaHome, FaLinkedin, FaMailBulk, FaPhone } from 'react-icons/fa'
+import { NavLink } from 'react-router-dom'
 
 const Footer = () => {
     return (
@@ -15,11 +16,11 @@ const Footer = () => {
                     </div>
                     <div className='phone'>
                         <h4><FaPhone size={20} style={{ color: "#fff", marginRight: "2rem" }} />
-                        +1 (678) 852-7809</h4>
+                            +1 (678) 852-7809</h4>
                     </div>
                     <div className='email'>
                         <h4><FaMailBulk size={20} style={{ color: "#fff", marginRight: "2rem" }} />
-                        rodneyedgejr@gmail.com</h4>
+                            rodneyedgejr@gmail.com</h4>
                     </div>
                 </div>
 
@@ -27,8 +28,12 @@ const Footer = () => {
                     <h4>About the software developer</h4>
                     <p>This is Rodney Edge, Jr. I am the owner of this portfolio. I will occasionally update with new projects.</p>
                     <div className='social'>
-                        <FaGithub size={30} style={{ color: "#fff", marginRight: "2rem" }} />
-                        <FaLinkedin size={30} style={{ color: "#fff", marginRight: "2rem" }} />
+                        <NavLink to='https://github.com/rmedge2' target='_blank'>
+                            <FaGithub size={30} style={{ color: "#fff", marginRight: "2rem" }} />
+                        </NavLink>
+                        <NavLink to='https://www.linkedin.com/in/rodneyedgejr/' target='_blank'>
+                            <FaLinkedin size={30} style={{ color: "#fff", marginRight: "2rem" }} />
+                        </NavLink>
                     </div>
                 </div>
             </div>
